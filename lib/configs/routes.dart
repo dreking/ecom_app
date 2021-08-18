af83515fa61ab5ece62bf5d79aa1155cca08deb1
Leandre <leajapo@gmail.com>
2021-08-18 02:53:07 +0200

    feat:added new configurations

--- lib/configs/routes.dart
+++ lib/configs/routes.dart
@@ -1,4 +1,7 @@
+import 'package:ecommerce/screens/cart_screen/cart_screen.dart';
 import 'package:ecommerce/screens/main_screen/main_screen.dart';
+import 'package:ecommerce/screens/order_screen/order_screen.dart';
+import 'package:ecommerce/screens/product_detail_screen/product_details_screen.dart';
 import 'package:ecommerce/screens/signinup/signinup_screen.dart';
 import 'package:ecommerce/screens/splash_screen/spash_screen.dart';
 
@@ -6,4 +9,7 @@ final routes = {
   SplashScreen.routeName: (ctx) => SplashScreen(),
   SignInUpScreen.routeName: (ctx) => SignInUpScreen(),
   MainScreen.routeName: (ctx) => MainScreen(),
+  ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+  CartScreen.routeName: (ctx) => CartScreen(),
+  OrderScreen.routeName: (ctx) => OrderScreen(),
 };
--- lib/data/get_urls.dart
+++ lib/data/get_urls.dart
@@ -3,5 +3,19 @@ String getServerDomain() {
 }
 
 String getProductsUrl() {
-  return 'https://asos2.p.rapidapi.com/products/v2/list?offset=0&limit=20&sort=freshness&categoryId=4209&lang=en-US&country=RW';
+  final Map<String, dynamic> queryParams = {
+    'offset': '0',
+    'limit': '20',
+    'sort': 'freshness',
+    'categoryId': '4209',
+    'lang': 'en-US',
+    'country': 'RW'
+  };
+  final String queryString = Uri(queryParameters: queryParams).query;
+
+  return 'https://asos2.p.rapidapi.com/products/v2/list?$queryString';
+}
+
+String getImageUrl(url) {
+  return 'https://$url';
 }
