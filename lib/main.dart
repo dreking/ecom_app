b8feb46e695b9e423cbcfb5c24c3565f2d330223
Leandre <leajapo@gmail.com>
2021-08-18 02:51:47 +0200

    feat:added new assets and packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:ecommerce/configs/routes.dart';
+import 'package:ecommerce/providers/auth_logic.dart';
+import 'package:ecommerce/providers/cart_logic.dart';
+import 'package:ecommerce/providers/order_logic.dart';
+import 'package:ecommerce/providers/products_logic.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,10 +13,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ecom',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthLogic()),
+        ChangeNotifierProvider(create: (_) => ProductLogic()),
+        ChangeNotifierProvider(create: (_) => CartLogic()),
+        ChangeNotifierProvider(create: (_) => OrderLogic()),
+      ],
+      child: MaterialApp(
+        title: 'Ecom',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+          primaryColor: Colors.blueGrey,
+        ),
+        routes: routes,
       ),
     );
   }
